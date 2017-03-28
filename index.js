fd2faa27ac180ee12abab065e9ecb664d4dbff2f
Juan Lulkin <maiz@lulk.in>
2017-03-28 20:06:10 +0300

    Adds main index

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http')
+const {on, stream} = require('flyd')
+const Result = require('folktale/data/result')
+const ok = require('./ok')
+
+const listen = (port, msg = `Listening to ${port}...`) => {
+  const $ = stream();
+
+  http
+    .createServer((req, res) => $(Result.of({ req, res, mid: {} })))
+    .listen(port, () => console.log(msg));
+
+  return $;
+};
+
+module.exports = {listen, on, ok};
